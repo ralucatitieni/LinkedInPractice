96ffa7cd37df0e0a5dea74f9b98851894217f613
raluca.titieni <raluca_titieni@neimanmarcus.com>
2023-11-01 09:50:44 +0200

    test password

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -5,16 +5,21 @@ import java.util.Scanner;
 public class App {
 
     public static boolean isPasswordComplex(String password) {
-        return false;
+        return password.length() >= 6 &&
+                password.chars().filter(Character::isDigit).count() >= 1 &&
+                password.chars().filter(Character::isUpperCase).count() >= 1 &&
+                password.chars().filter(Character::isLowerCase).count() >= 1;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Enter a password: ");
-        String userInput = scanner.nextLine();
-        System.out.println("Is the password complex? "
-                + isPasswordComplex(userInput));
+        boolean again = true;
+        while (again) {
+            System.out.println("Enter a password: ");
+            String userInput = scanner.nextLine();
+            System.out.println("Is the password complex? "
+                    + isPasswordComplex(userInput));
+        }
 
         scanner.close();
     }
