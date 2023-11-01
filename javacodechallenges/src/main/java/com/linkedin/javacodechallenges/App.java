7a88a8d2fecf94cadf1d9972e0e32e780a4e2739
raluca.titieni <raluca_titieni@neimanmarcus.com>
2023-11-01 14:58:47 +0200

    add 100 days

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,16 @@
 package com.linkedin.javacodechallenges;
 
-public class App 
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+public class App
 {
     // Create function to calculate the date that's 
     // 100 days from now
 
     public static void main( String[] args )
     {
-        System.out.println("100 days from now is... " 
-            /* add function call */);
+        System.out.println("100 days from now is... " +
+                LocalDate.now().plusDays(100));
     }
 }
