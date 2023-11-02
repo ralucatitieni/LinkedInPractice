2171b916dd73cc60209cffdf695dc8cf59548ab5
raluca.titieni <raluca_titieni@neimanmarcus.com>
2023-11-02 10:38:56 +0200

    replace word in file

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,11 +1,43 @@
 package com.linkedin.javacodechallenges;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class App 
-{
+import org.apache.commons.lang3.StringUtils;
+
+
+public class App {
+
     public static void redactTextFile(String fileName,
-                                      String[] redactedWordsArray) {
+            String[] redactedWordsArray) {
+
+        File file = new File(fileName);
+        try (var in = new BufferedReader(new FileReader(file));
+                var out = new BufferedWriter(new FileWriter(new File("lalala.txt")))) {
+            String line = "";
+            while ((line = in.readLine()) != null) {
+                for (String toBeReplaced: redactedWordsArray){
+                   line =  StringUtils.replaceIgnoreCase(line,toBeReplaced, "REDACTED");
+                }
+                out.write(line);
+                out.newLine();
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
